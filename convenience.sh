5b778c84b7b6c024ef8bceb99bff044c69f5dd2a
earthexe <108993421+earthexe@users.noreply.github.com>
2022-07-30 10:02:42 -0700

    Create convenience.sh

---
+++ convenience.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo "This computer will be rebooted after the convenience script has finished working!"
+sleep 10
+pacman -S packagekit-qt5
+pacman -S alsa-firmware sof-firmware alsa-ucm-conf dhcpcd networkmanager iwd dhcpcd alsa-utils
+systemctl stop dhcpcd
+systemctl stop wpa_supplicant
+systemctl disable dhcpcd
+systemctl stop wpa_supplicant
+systemctl enable iwd
+systemctl start iwd
+systemctl start NetworkManager
+systemctl enable NetworkManager 
